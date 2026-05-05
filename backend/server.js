dc76622041828d006bedadff78d1725440b00902
Chaithanya-ise24 <chaithanya.ise24@cmrit.ac.in>
2026-05-05 23:19:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -63,7 +63,7 @@ app.post('/upload', upload.single('pdf'), (req, res) => {
     }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
